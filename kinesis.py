4f0df94c5274f1ec7c7e9d05295ee1a31ebc45de
edmondztt <edmondztt@users.noreply.github.com>
2024-02-18 23:22:54 -0800

    try pipeline so far

---
+++ kinesis.py
@@ -0,0 +1,135 @@
+import hoomd
+import matplotlib
+import itertools
+import math
+import gsd.hoomd
+import numpy as np
+from random import randint
+from random import uniform
+import datetime, time
+
+
+matplotlib.style.use('ggplot')
+import matplotlib_inline
+
+matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
+
+class print_sim_state(hoomd.custom.Action):
+    def act(self, timestep):
+        global init_time, last_output
+        try:
+            sec_remaining = int((self._state._simulation.final_timestep - timestep) / self._state._simulation.tps)
+        except ZeroDivisionError:
+            sec_remaining = 0
+        print(
+            "Time", str(datetime.timedelta(seconds=int(time.time() - init_time))),
+            "| Step:", timestep,
+            "| TPS:", str(round(float(self._state._simulation.tps),3)),
+            "| ETR:", str(datetime.timedelta(seconds=sec_remaining))
+        )
+        last_output = time.time()
+
+class trigger_every_n_sec(hoomd.trigger.Trigger):
+    def __init__(self):
+        hoomd.trigger.Trigger.__init__(self)
+
+    def compute(self, timestep):
+        global last_output, update_every_n
+        return ((time.time() - last_output) >=update_every_n)
+
+def rand_unit_quaternion(N, threeD=False):
+    '''creates a unit quaternion to describe a 2 or 3 dimensional object; returns [w, x, y, z]'''
+    orientation = []
+    for i in range(N):
+        roll = 0 if threeD == 0 else np.pi * uniform(-1,1) # rotation around x-axis
+        pitch = 0 if threeD == 0 else np.pi * uniform(-1,1)# rotation around y-axis
+        yaw = np.pi * uniform(-1,1) # rotation around z-axis
+        v = np.array(
+            [
+                np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2), #qw
+                np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2), #qx
+                np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2), #qy
+                np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) #qz
+            ]
+        )
+        v = v / np.linalg.norm(v)
+        orientation.append(np.array(v))
+    return orientation
+
+
+
+dt = 1e-3
+sigma_tumble = 0.1
+DR = 0.5
+U0 = 5.0
+runtime = 100
+
+gsd_filename = 'test.gsd'
+
+m = 4
+N_particles = 4 * m**2
+
+spacing = 1.3
+K = math.ceil(N_particles ** (1 / 2))
+L = K * spacing
+x = np.linspace(-L / 2, L / 2, K, endpoint=False)
+X, Y = np.meshgrid(x,x)
+X = X.flatten()
+Y = Y.flatten()
+Z = np.zeros_like(X)
+position = np.stack((X,Y,Z),axis=-1)
+
+frame = gsd.hoomd.Frame()
+frame.particles.N = N_particles
+frame.particles.position = position[0:N_particles]
+frame.particles.typeid = [0] * N_particles
+frame.configuration.box = [L, L, 1, 0, 0, 0]
+frame.particles.types = ['A']
+frame.particles.orientation = rand_unit_quaternion(N_particles)
+
+with gsd.hoomd.open(name='init.gsd', mode='x') as f:
+    f.append(frame)
+
+cpu = hoomd.device.CPU()
+simulation = hoomd.Simulation(device=cpu, seed=1)
+simulation.create_state_from_gsd(
+    filename='init.gsd'
+)
+
+integrator = hoomd.md.Integrator(dt=0.005)
+cell = hoomd.md.nlist.Cell(buffer=0.4)
+lj = hoomd.md.pair.LJ(nlist=cell)
+lj.params[('A', 'A')] = dict(epsilon=0, sigma=0.1)
+lj.r_cut[('A', 'A')] = 0.1
+integrator.forces.append(lj)
+# nvt = hoomd.md.methods.ConstantVolume(
+#     filter=hoomd.filter.All(), thermostat=hoomd.md.methods.thermostats.Bussi(kT=1.5)
+# )
+# integrator.methods.append(nvt)
+
+mixed_active = hoomd.md.force.MixedActive(filter=hoomd.filter.All())
+mixed_active.mixed_active_force['A','A'] = (1,0,0)
+mixed_active.active_torque['A','A'] = (0,0,0)
+rotational_diffusion_tumble_updater = mixed_active.create_diffusion_tumble_updater(
+    trigger=10, rotational_diffusion=DR, tumble_angle_gauss_spread=sigma_tumble)
+simulation.operations += rotational_diffusion_tumble_updater
+
+integrator.forces.append(mixed_active)
+simulation.operations.integrator = integrator
+
+
+update_every_n = 30
+simulation.operations.writers.append(
+    hoomd.write.CustomWriter(
+        action = print_sim_state(),
+        trigger = trigger_every_n_sec()
+    )
+)
+
+gsd_writer = hoomd.write.GSD(trigger=hoomd.trigger.Periodic(1_000),
+                      filename=gsd_filename)
+simulation.operations.writers.append(gsd_writer)
+
+init_time = time.time()
+last_output = init_time
+simulation.run(int(runtime/dt))
