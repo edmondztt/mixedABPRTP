1ce6d3017fc3bbb765155720ca8f444e96047299
edmondztt <edmondztt@users.noreply.github.com>
2024-03-14 01:17:37 -0700

    no verb

--- md/MixedActiveForceCompute.cc
+++ md/MixedActiveForceCompute.cc
@@ -667,8 +667,8 @@ void MixedActiveForceCompute::update_dynamical_parameters(uint64_t timestep){
         h_S.data[idx] = S;
         h_U.data[idx] = U;
         h_tumble_rate.data[idx] = gamma;
-        if(timestep%10000==0)
-            printf("now at timestep %d, part %d type %d:  c_new=%g, h_c=%g, h_U=%g, updated QH=%g,QT=%g,gamma=%g,S=%g, U=%g after update dynamical\n", timestep, idx, typ, c_new, h_c.data[idx], h_U.data[idx], QH, QT, gamma, S, U);
+        // if(timestep%10000==0)
+        //     printf("now at timestep %d, part %d type %d:  c_new=%g, h_c=%g, h_U=%g, updated QH=%g,QT=%g,gamma=%g,S=%g, U=%g after update dynamical\n", timestep, idx, typ, c_new, h_c.data[idx], h_U.data[idx], QH, QT, gamma, S, U);
     }
     // printf("\n\n");
 }
