6a55dfa343065e2280d8030d8cb64755b6b48b5f
edmondztt <edmondztt@users.noreply.github.com>
2024-03-13 19:45:02 -0700

    dbging

--- md/MixedActiveForceCompute.h
+++ md/MixedActiveForceCompute.h
@@ -153,7 +153,7 @@ public:
             double value;
             int time = 0; // Assuming time starts at 0 and increments in fixed intervals as per the file format
             while (iss >> value) {
-                if(r<0.5 && theta>5 && time==100){
+                if(r<2 && time==100){
                     printf("now reading c: @r=%g,theta=%g,time=100: c=%g\n",value);
                 }
                 this->setData(r, theta, time, value);
