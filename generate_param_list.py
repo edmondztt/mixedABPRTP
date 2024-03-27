ec13012d6eee222dbe2a89765951edcd7d552f22
edmondztt <edmondztt@users.noreply.github.com>
2024-03-26 19:55:21 -0700

    choose k so that at steady state Q~O(1)

--- generate_param_list.py
+++ generate_param_list.py
@@ -6,18 +6,18 @@ import numpy as np
 Np = int(100)
 runtime = 500
 Q0 = 0.3
-kT2 = 1.0
-kH2 = 1.0
-kS2 = 0.01
+kT20 = 1.0/30
+kH20 = 1.0/30
+kS20 = 0.1
 iftaxis = "true"
 
 fname = 'parameters.csv'
 with open(fname, 'w') as f:
     f.write('# N_particles runtime Q0 Q1 kT2 kH2 kS2 iftaxis ifkinesis\n')
-    for kT2 in [1.0, 5.0]:
-        for kH2 in [1.0, 5.0]:
-            for kS2 in [0.1]:
-                for Q1 in [5.0, 10.0, 50.0]:
+    for kT2 in [1.0, 5.0]*kT20:
+        for kH2 in [1.0, 5.0]*kH20:
+            for kS2 in [1.0]*kS20:
+                for Q1 in [1.0, 5.0]:
                     for ifkinesis in ["false","true"]:
                         params = "{Np:d},{runtime:d},{Q0},{Q1},{kT2},{kH2},{kS2},{iftaxis},{ifkinesis}".format(Np=Np,runtime=runtime,
                             Q0=Q0,Q1=Q1,kT2=kT2,kH2=kH2,kS2=kS2,iftaxis=iftaxis,ifkinesis=ifkinesis)
