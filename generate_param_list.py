b132a4c17ffebbfeed93528448a997aa19ce40f8
edmondztt <edmondztt@users.noreply.github.com>
2024-03-27 12:45:01 -0700

    refine params and response

--- generate_param_list.py
+++ generate_param_list.py
@@ -17,7 +17,7 @@ with open(fname, 'w') as f:
     for kT2 in np.array([1.0, 5.0])*kT20:
         for kH2 in np.array([1.0, 5.0])*kH20:
             for kS2 in np.array([1.0])*kS20:
-                for Q1 in [1.0]:
+                for Q1 in [0.9]:
                     for ifkinesis in ["false","true"]:
                         params = "{Np:d},{runtime:d},{Q0},{Q1},{kT2},{kH2},{kS2},{iftaxis},{ifkinesis}".format(Np=Np,runtime=runtime,
                             Q0=Q0,Q1=Q1,kT2=kT2,kH2=kH2,kS2=kS2,iftaxis=iftaxis,ifkinesis=ifkinesis)
