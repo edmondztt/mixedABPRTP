043b6071520be68dbfb6eec3442b2fab30fef886
edmond <edmondztt@gmail.com>
2024-04-11 21:36:18 -0700

    save

--- generate_param_list.py
+++ generate_param_list.py
@@ -23,7 +23,7 @@ with open(fname, 'w') as f:
         # for Q0 in [0.5, 1.0, 0.2]:
         # for noise_Q in [0.1, 1.0]:
         for noise_Q in [0.1, 0.5, 1.0]:
-            for k2factor in np.array([0.2, 0.8, 1.6]):
+            for k2factor in np.array([0.2, 0.5, 1.0, 2.0]):
                     kHT2 = kHT20 * k2factor
                     # for kklino in [1.0]:
                     for DR in [1/10]:
